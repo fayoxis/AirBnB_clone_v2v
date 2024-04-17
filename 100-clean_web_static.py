7afb093ff5683940d03fedf6cd69791595967383
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-17 15:35:01 +0100

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-""" Function that deploys """
-from fabric.api import *
+import os
+import shutil
+from fabric.api import local, run, env
 
-
+# Set the hosts and user for the remote servers
 env.hosts = ['18.209.224.119', '54.157.173.122']
 env.user = "ubuntu"
 
+def clean_releases(keep_count=2):
+    """Clean up old releases on both local and remote servers"""
+    clean_local_releases(keep_count)
+    clean_remote_releases(keep_count)
 
-def do_clean(number=0):
-    """ CLEANS """
+def clean_local_releases(keep_count):
+    """Clean up old releases on the local machine"""
+    versions_dir = "versions"
+    if os.path.exists(versions_dir):
+        local_releases = sorted(os.listdir(versions_dir), reverse=True)
+        for release in local_releases[keep_count:]:
+            release_path = os.path.join(versions_dir, release)
+            shutil.rmtree(release_path)
 
-    number = int(number)
-
-    if number == 0:
-        number = 2
-    else:
-        number += 1
-
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+def clean_remote_releases(keep_count):
+    """Clean up old releases on the remote servers"""
+    releases_path = "/data/web_static/releases"
+    with cd(releases_path):
+        remote_releases = sorted(run("ls -t").stdout.strip().split("\n"), reverse=True)
+        for release in remote_releases[keep_count:]:
+            run("rm -rf {}".format(os.path.join(releases_path, release)))
