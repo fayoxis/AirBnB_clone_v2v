d427e1ffa9dbdcc51d11e8890fddb413cc6c54d2
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-17 10:58:19 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,37 @@
 #!/usr/bin/env bash
-# Bash script that sets up web servers for the deployment of web_static
+# Update package information
 sudo apt-get update
+
+# Install Nginx
 sudo apt-get -y install nginx
+
+# Allow Nginx HTTP traffic
 sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
+# Create necessary directories
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+sudo mkdir -p /data/web_static/shared/
+
+# Create index.html file with desired content
+sudo bash -c 'cat > /data/web_static/releases/test/index.html << EOF
+<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>
+EOF'
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+# Create symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
+# Change ownership of directories
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# Add configuration for serving static files
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}'
+/etc/nginx/sites-enabled/default
 
+# Restart Nginx service
 sudo service nginx restart
