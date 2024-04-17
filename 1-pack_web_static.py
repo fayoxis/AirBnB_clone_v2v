fe1bd68ebc51fa0f24e70931c9359d4bcdf94806
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-17 13:27:16 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,28 +2,33 @@
 """A module for Fabric script that generates a .tgz archive."""
 import os
 from datetime import datetime
-from fabric.api import local, runs_once
+from fabric.api import task
 
-
-@runs_once
-def do_pack():
+@task
+def archive_files():  
     """Archives the static files."""
+    output_file = get_archive_filename()
+    
     if not os.path.isdir("versions"):
         os.mkdir("versions")
-    d_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        d_time.year,
-        d_time.month,
-        d_time.day,
-        d_time.hour,
-        d_time.minute,
-        d_time.second
-    )
+
     try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, size))
+        print(f"Packing web_static to {output_file}")  
+        run(f"tar -czf {output_file} web_static")
+        size = get_file_size(output_file)
+        print(f"web_static packed: {output_file} -> {size} Bytes")
     except Exception:
-        output = None
-    return output
+        output_file = None
+        
+    return output_file
+    
+def get_archive_filename():
+   now = datetime.now()
+   filename = f"versions/web_static_{now.year}{now.month}{now.day}{now.hour}{now.minute}{now.second}.tgz"
+   return filename
+   
+def get_file_size(filename):
+   return os.stat(filename).st_size
+   
+def run(command):
+   local(command)
