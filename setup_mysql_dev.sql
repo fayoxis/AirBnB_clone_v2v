5bbe7cdb9064fe630f1afa8e34307ba921f59d16
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-17 15:17:49 +0100

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,13 @@
 -- This script sets up a MySQL server for a project
--- Create the project development database with the name 'hbnb_dev_db'
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Create a new user named 'hbnb_dev' with all privileges on the 'hbnb_dev_db' database
--- The password for this user is 'hbnb_dev_pwd' if the user doesn't already exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Create the project development database if it doesn't exist
+DROP DATABASE IF EXISTS hbnb_dev_db;
+CREATE DATABASE hbnb_dev_db;
 
--- Grant all privileges on the 'hbnb_dev_db' database to the user 'hbnb_dev'
+-- Create a new user 'hbnb_dev' with the password 'hbnb_dev_pwd' if it doesn't exist
+-- Grant all privileges on the 'hbnb_dev_db' database to this user
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 
