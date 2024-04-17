2cca01e707e54cfbcc5b0ee7e07beb1fe88e630f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-17 15:38:07 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,51 @@
 #!/usr/bin/python3
-"""Distributes an archive to your web servers, using the function do_deploy"""
-from fabric.contrib import files
-from fabric.api import env, put, run
+"""this archive to your web servers, using the function do_deploy"""
+from fabric.api import env, run, put, local, cd
+from contextlib import contextmanager
 import os
 
 env.hosts = ['18.209.224.119', '54.157.173.122']
 
+@contextmanager
+def source_code():
+    """Context manager to create a temporary directory for the source code."""
+    directory = run('mktemp -d --tmpdir=/tmp')
+    try:
+        yield directory
+    finally:
+        run(f'rm -rf {directory}')
 
 def do_deploy(archive_path):
-    """Function for deploy"""
+    """Distribute an archive to the web servers."""
     if not os.path.exists(archive_path):
         return False
 
-    data_path = '/data/web_static/releases/'
-    tmp = archive_path.split('.')[0]
-    name = tmp.split('/')[1]
-    dest = data_path + name
+    with source_code() as tmp_dir:
+        # Upload the archive to the temporary directory
+        archive_name = os.path.basename(archive_path)
+        put(archive_path, f'{tmp_dir}/{archive_name}')
 
-    try:
-        put(archive_path, '/tmp')
-        run('mkdir -p {}'.format(dest))
-        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
-        run('rm -f /tmp/{}.tgz'.format(name))
-        run('mv {}/web_static/* {}/'.format(dest, dest))
-        run('rm -rf {}/web_static'.format(dest))
+        # Extract the archive
+        with cd(tmp_dir):
+            run(f'tar -xzf {archive_name}')
+
+        # Get the name of the extracted directory
+        extracted_dir = run(f'ls {tmp_dir}').stdout.strip()
+
+        # Create the release directory
+        data_path = '/data/web_static/releases/'
+        run(f'mkdir -p {data_path}')
+        release_dir = f'{data_path}{extracted_dir}'
+
+        # Move the extracted files to the release directory
+        run(f'mv {tmp_dir}/{extracted_dir} {release_dir}')
+
+        # Remove the current symbolic link, if it exists
         run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(dest))
+
+        # Create a new symbolic link
+        run(f'ln -s {release_dir} /data/web_static/current')
+
         return True
-    except Exception:
-        return False
+
+    return False
