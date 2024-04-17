ed6323b48e8c3d91a020ab1648be1eb35e9faaec
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-17 15:36:09 +0100

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,87 +1,91 @@
 # Puppet for setup
-
-$nginx_conf = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By ${hostname};
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
+$nginx_conf = @("CONF"/L)
+    server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        add_header X-Served-By ${hostname};
+        root /var/www/html;
         index index.html index.htm;
+
+        location /hbnb_static {
+            alias /data/web_static/current;
+            index index.html index.htm;
+        }
+
+        location /redirect_me {
+            return 301 https://hbibidunni.com/;
+        }
+
+        error_page 404 /404.html;
+        location /404 {
+            root /var/www/html;
+            internal;
+        }
     }
-    location /redirect_me {
-        return 301 https://hbibidunni.com/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}"
+    CONF
 
-package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt'
+class { 'nginx': }
+
+file { '/data':
+  ensure => 'directory',
 }
 
--> file { '/data':
-  ensure  => 'directory'
+file { '/data/web_static':
+  ensure => 'directory',
 }
 
--> file { '/data/web_static':
-  ensure => 'directory'
+file { '/data/web_static/releases':
+  ensure => 'directory',
 }
 
--> file { '/data/web_static/releases':
-  ensure => 'directory'
+file { '/data/web_static/releases/test':
+  ensure  => 'directory',
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n",
 }
 
--> file { '/data/web_static/releases/test':
-  ensure => 'directory'
+file { '/data/web_static/shared':
+  ensure => 'directory',
 }
 
--> file { '/data/web_static/shared':
-  ensure => 'directory'
-}
-
--> file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
-}
-
--> file { '/data/web_static/current':
+file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test'
+  target => '/data/web_static/releases/test',
 }
 
--> exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path        => '/usr/bin:/usr/local/bin:/bin/',
+  refreshonly => true,
+  subscribe   => File['/data/web_static/releases/test'],
 }
 
 file { '/var/www':
-  ensure => 'directory'
+  ensure => 'directory',
 }
 
--> file { '/var/www/html':
-  ensure => 'directory'
+file { '/var/www/html':
+  ensure  => 'directory',
+  recurse => true,
+  purge   => true,
+  force   => true,
 }
 
--> file { '/var/www/html/index.html':
+file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "This is my first upload  in /var/www/index.html***\n"
+  content => "This is my first upload in /var/www/index.html***\n",
 }
 
--> file { '/var/www/html/404.html':
+file { '/var/www/html/404.html':
   ensure  => 'present',
-  content => "Ceci n'est pas une page - Error page\n"
+  content => "Ceci n'est pas une page - Error page\n",
 }
 
--> file { '/etc/nginx/sites-available/default':
+file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
-  content => $nginx_conf
+  content => $nginx_conf,
+  notify  => Service['nginx'],
 }
 
--> exec { 'nginx restart':
-  path => '/etc/init.d/'
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
 }
